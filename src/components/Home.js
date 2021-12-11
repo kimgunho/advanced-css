0870ffdbdf51a38b68103823438bf71789ce8959
kim <yuyukgh@naver.com>
2021-12-12 07:01:38 +0900

    feat: add home component & style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
+import classNames from 'classnames/bind';
+
+import styles from './Home.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Home() {
   return (
-    <div>
-      {/* # Advanced CSS3 1. [Loading] CSS Animation Effects - 3 rotating lines */}
+    <div className={cx('container')}>
+      <h1 className={cx('title')}>
+        Not javaScript <br /> <span>just</span> use <span>CSS3</span> <br />
+        for <span>simple</span>
+      </h1>
     </div>
   );
 }
